d9a8fad4129c871359f088b5ddd568cb4814271e
Giovanni 'Giotino' Minotti <giovanni@minotti.io>
2023-09-19 10:22:39 +0200

    Fixed http example

--- game-archive/http/index.js
+++ game-archive/http/index.js
@@ -19,9 +19,10 @@ module.exports = {
   teams,
   submitFlags: async (flags, onSubmit) => {
     const tot = flags.length;
-    const chunkSize = 20;
-    for (let i = 0; i < tot - chunkSize; i += chunkSize) {
-      const answer = await got
+    const chunkSize = Math.min(20, flags.length);
+    for (let i = 0; i < tot; i += chunkSize) {
+      try {
+        const answer = await got
         .put(SYSTEM_IP, {
           headers: {
             'X-Team-Token': TEAM_TOKEN
@@ -31,8 +32,11 @@ module.exports = {
         })
         .json();
 
-      for (const a of answer) {
-        await onSubmit(a.flag, a.status ? 'ACCEPTED' : 'REJECTED', a.msg.split(']')[1]);
+        for (const a of answer) {
+          await onSubmit(a.flag, a.status ? 'ACCEPTED' : 'REJECTED', a.msg.split(']')[1]);
+        }
+      } catch (e) {
+        console.log(e);
       }
     }
   }
